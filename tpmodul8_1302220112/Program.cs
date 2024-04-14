a8b20d3c812021a500f7e29c12ff2ad4e5e604f7
Syauqi <syauqi.d.u@gmail.com>
2024-04-15 03:26:08 +0700

    menyelesaikan program dengan runtime configuration

--- tpmodul8_1302220112/Program.cs
+++ tpmodul8_1302220112/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+class CovidConfig
+{
+    public string SatuanSuhu { get; set; }
+    public int BatasHariDemam { get; set; }
+    public string PesanDitolak { get; set; }
+    public string PesanDiterima { get; set; }
+
+    public CovidConfig()
+    {
+        // Set nilai default
+        SatuanSuhu = "celcius";
+        BatasHariDemam = 14;
+        PesanDitolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        PesanDiterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+    }
+
+    public void BacaKonfigurasi(string path)
+    {
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            CovidConfig config = JsonConvert.DeserializeObject<CovidConfig>(json);
+            SatuanSuhu = config.SatuanSuhu;
+            BatasHariDemam = config.BatasHariDemam;
+            PesanDitolak = config.PesanDitolak;
+            PesanDiterima = config.PesanDiterima;
+        }
+    }
+
+    public void UbahSatuan()
+    {
+        SatuanSuhu = SatuanSuhu == "celcius" ? "fahrenheit" : "celcius";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Membaca konfigurasi dari file
+        CovidConfig config = new CovidConfig();
+        config.BacaKonfigurasi("covid_config.json");
+
+        // Meminta input dari pengguna
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.SatuanSuhu}: ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hariDemam = Convert.ToInt32(Console.ReadLine());
+
+        // Memeriksa kondisi
+        bool kondisiSuhu = (config.SatuanSuhu == "celcius" && suhu >= 36.5 && suhu <= 37.5) ||
+                           (config.SatuanSuhu == "fahrenheit" && suhu >= 97.7 && suhu <= 99.5);
+        bool kondisiHari = hariDemam < config.BatasHariDemam;
+
+        // Mengeluarkan output sesuai kondisi
+        if (kondisiSuhu && kondisiHari)
+        {
+            Console.WriteLine(config.PesanDiterima);
+        }
+        else
+        {
+            Console.WriteLine(config.PesanDitolak);
+        }
+
+        // Panggil method untuk mengubah satuan suhu
+        config.UbahSatuan();
+        Console.WriteLine($"Satuan suhu telah diubah menjadi: {config.SatuanSuhu}");
+    }
+}
--- tpmodul8_1302220112/obj/Debug/net8.0/tpmodul8_1302220112.AssemblyInfo.cs
+++ tpmodul8_1302220112/obj/Debug/net8.0/tpmodul8_1302220112.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul8_1302220112")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4591f006345e6a7afb9e04e0c52013381639bd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul8_1302220112")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul8_1302220112")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
